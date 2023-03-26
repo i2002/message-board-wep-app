011dceefcd242dbdeb29a182ce94b59739a87f05
Tudor Butufei <tudor.butufei@gmail.com>
2023-03-26 18:06:10 +0300

    create Topics MVC scaffolding

---
+++ Data/MessageBoardContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MessageBoard.Models;
+
+namespace MessageBoard.Data
+{
+    public class MessageBoardContext : DbContext
+    {
+        public MessageBoardContext (DbContextOptions<MessageBoardContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MessageBoard.Models.Topic> Topic { get; set; } = default!;
+    }
+}
---
+++ Models/Topic.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MessageBoard.Models
+{
+    public class Topic
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        [DataType(DataType.DateTime)]
+        public DateTime CreatedDate { get; set;}
+        [DataType(DataType.DateTime)]
+        public DateTime UpdatedDate { get; set;}
+        public string? Content { get; set; }
+        public string? Category { get; set; }
+    }
+}
